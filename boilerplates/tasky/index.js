24efa4033d5a2e9272ba5443b0d3654964c742c1
AdamMarczyk <adam.marczyk90@gmail.com>
2018-05-24 02:15:23 +0200

    move functionality

--- boilerplates/tasky/index.js
+++ boilerplates/tasky/index.js
@@ -21,25 +21,5 @@ app.on('ready', () => {
     ? 'windows-icon.png'
     : 'iconTemplate.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`)
-  tray = new TimerTray(iconPath);
-  tray.on('click', (event, bounds) => {
-    const { x, y } = bounds;
-    const { height, width } = mainWindow.getBounds();
-
-    if (mainWindow.isVisible()) {
-      mainWindow.hide();
-    }
-    else {
-      const yPosition = process.platform === 'darwin'
-        ? y
-        : y - height;
-      mainWindow.setBounds({
-        x: x - width / 2,
-        y: yPosition,
-        height,
-        width,
-      });
-      mainWindow.show();
-    };
-  });
+  tray = new TimerTray(iconPath, mainWindow);
 });
